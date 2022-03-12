45d66acf301f8db30b63e9f2be4fe2aeb7f68939
JAISHRI <sjayashree529@gmail.com>
2022-03-12 22:34:43 +0530

    UC-1

--- src/com/bridgelab/addressbook/AddressBookMain.java
+++ src/com/bridgelab/addressbook/AddressBookMain.java
@@ -5,6 +5,11 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Wel come to Address Book ");
+		AddressBook contactDetails = new AddressBook();
+        //contactDetails.addContact();
+        //contactDetails.searchExistingContact();
+        //contactDetails.deleteExistingContact();
+        //contactDetails.addMultiplePerson();
 
 	}
 
