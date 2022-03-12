fce67bab67a1a1e818835b256564ca1a1cca321a
JAISHRI <sjayashree529@gmail.com>
2022-03-12 17:09:11 +0530

    welcome

---
+++ src/com/bridgelab/addressbook/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelab.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wel come to Address Book ");
+
+	}
+
+}
